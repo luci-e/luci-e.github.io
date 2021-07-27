56ad6cb0e7a6c186ff076542f136cba20adf76eb
Emiliano Luci <luciemiliano@gmail.com>
2021-07-27 13:22:27 +0200

    close bee

--- hive/script/script.js
+++ hive/script/script.js
@@ -9,12 +9,38 @@ function flowerify(){
 		f.attr("src", `img\\flower (${Math.floor(Math.random() * flowersNo) + 1}).png`);
 		f.css("top", `${Math.floor(Math.random() * 150) -50 }vh`)
 		f.css("left", `${Math.floor(Math.random() * 150) -50 }vw`)
-		f.css("animation-delay", `${Math.floor(Math.random() * 3000) + 1}ms`)
+		f.css("animation-delay", `-${Math.floor(Math.random() * 3000) + 1}ms`)
 		body.append(f);
 	}
+}
+
+var decrypted = false;
+var enc = null;
+
+function handleSubmit(event){
+	event.preventDefault();
+	console.log( $("#passcode").val());
+
+	if (!decrypted){
+		let dec = CryptoJS.AES.decrypt(enc, $("#passcode").val() ).toString(CryptoJS.enc.Utf8);
+
+		if(dec.startsWith("text")){
+			decrypted = true;
+			$("#bee_says").html("Thanks, here are the coordinates:")
+		}else{
+			$("#bee_says").html("Bzz Bzz that was wrong")
+		}
+	}
+
+	return false;
 }
 
 $(document).ready( function() {
 	console.log("ready!");
 	flowerify();
+
+	$("#submit_button").on("click", handleSubmit);
+
+	enc = CryptoJS.AES.encrypt("text", "password").toString();
+	console.log(enc);
 })
