ff59939f41403033e2feb00098c42c3e7109191c
Emiliano Luci <luciemiliano@gmail.com>
2023-07-05 16:29:29 +0200

    i have no idea what i'm doing

--- projects/personal-page/src/pageroot/personal-page.ts
+++ projects/personal-page/src/pageroot/personal-page.ts
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
+const logo = new URL('../../../assets/open-wc-logo.svg', import.meta.url).href;
 
 @customElement('personal-page')
 export class PersonalPage extends LitElement {
